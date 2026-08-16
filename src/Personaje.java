0bbacd6a4094b7c32855137e80fe0729f1dcef9c
Aldana Godoy <godoyaldanasofia@gmail.com>
2026-08-16 20:37:30 -0300

    Peronaje: agregue atributos, constructor, setters y getters

--- src/Personaje.java
+++ src/Personaje.java
@@ -1,4 +1,56 @@
 public class Personaje {
-    private string nombre;
-    private Int puntosVida;
+
+    // -- ATRIBUTOS --
+    private String nombre;
+    private int puntosVida;
+    private int nivelEstamina;
+    // private armaPersonaje;
+    // private linterna
+
+    // -- CONSTRUCTOR --
+    public Personaje (String nombre) {
+        this.nombre = nombre;
+        this.puntosVida = 100;
+        this.nivelEstamina = 100;
+    }
+
+    // -- SET's y GET's --
+    public String getNombre () {
+        return nombre;
+    }
+
+    public int getPuntosVida (){
+        return puntosVida;
+    }
+
+    public int getNivelEstamina(){
+        return nivelEstamina;
+    }
+
+    public void setPuntosVida(int puntosVida) {
+        if (this.puntosVida < 0 ) {
+            puntosVida = 0;
+        } else if (this.puntosVida > 100) {
+            puntosVida = 100;
+        } else {
+            this.puntosVida = puntosVida;
+        }
+
+    }
+
+    public void setNivelEstamina (int nivelEstamina) {
+        if (this.nivelEstamina < 0) {
+            nivelEstamina = 0;
+        } else if (this.nivelEstamina > 100) {
+            nivelEstamina = 100;
+        } else {
+            this.nivelEstamina = nivelEstamina;
+        }
+    }
+
+
+
+
+
+
 }
