897d395032fbbc362532940c8ad83ad7f433dc10
Aldana Godoy <godoyaldanasofia@gmail.com>
2026-08-22 19:10:46 -0300

    Modifique la superclase Entidad

--- src/Entidad.java
+++ src/Entidad.java
@@ -1,66 +1,39 @@
 public abstract class Entidad {
 
     // -- ATRIBUTOS --
-    //private String nombre;
     protected int puntosVida;
-    //private int nivelEstamina;
-    // private armaPersonaje;
-    // private linterna
 
     // -- CONSTRUCTOR --
     public Entidad (int puntosVida) {
-        //this.nombre = nombre;
         this.puntosVida = 100;
-        //this.nivelEstamina = 100;
     }
 
     // -- SET's y GET's --
-    //public String getNombre () {
-        ///
-        /// }
-
     public int getPuntosVida (){
         return puntosVida;
     }
 
-    //public int getNivelEstamina(){
-       // return nivelEstamina;
-    //}
 
     public void setPuntosVida(int puntosVida) {
-        if (this.puntosVida < 0 ) {
-            puntosVida = 0;
-        } else if (this.puntosVida > 100) {
-            puntosVida = 100;
+        if (puntosVida < 0 ) {
+            this.puntosVida = 0;
+        } else if (puntosVida > 100) {
+            this.puntosVida = 100;
         } else {
             this.puntosVida = puntosVida;
         }
 
     }
 
-    /*
-    public void setPuntosVida (int puntosVida) {
-    if (puntosVida < this.puntosVida) {
-    puntosVida=this.puntosVida;
-    } else if (puntosVida > this.puntosVida){
-    puntosVida = this.puntosVida;
-    } else {
-    this.puntosVida =puntosVida;
-    }
-     */
+    // -- METODOS --
 
-    /*
-    public void setNivelEstamina (int nivelEstamina) {
-        if (this.nivelEstamina < 0) {
-            nivelEstamina = 0;
-        } else if (this.nivelEstamina > 100) {
-            nivelEstamina = 100;
-        } else {
-            this.nivelEstamina = nivelEstamina;
-        }
+    public void recibirDanio (int cantidad){
+        setPuntosVida(this.puntosVida - cantidad);
     }
 
-     */
+    public abstract void atacar(Entidad objetivo);  // método SIN cuerpo, termina en ";" — cada subclase decide cómo atacar
+
+
 
 
 
