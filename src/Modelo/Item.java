944fbab88e937bd158976049045d8b6c980599a1
Iskari Pardo <iskaripardo444@gmail.com>
2026-08-23 23:24:43 -0300

    se deberia haber agregado item.java

---
+++ src/Modelo/Item.java
@@ -0,0 +1,24 @@
+public class Item {
+// -- ATRIBUTOS --
+    protected boolean recogido;
+
+    public Item (){
+        this.recogido = false;
+    }
+
+// -- GET y SET --
+
+    public boolean isRecogido(){
+        return recogido;
+    }
+
+    public void setRecogido(boolean valor){
+        recogido = valor;
+    }
+
+// -- METODOS --
+
+    public void recoger(){
+       recogido = true;
+    }
+}
