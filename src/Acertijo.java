07fe8698abd3bda5b46982db23c2d3b9e1d27b32
Aldana Godoy <godoyaldanasofia@gmail.com>
2026-08-23 21:54:28 -0300

    Agregue la clase Acertijo

--- src/Acertijo.java
+++ src/Acertijo.java
@@ -1,4 +1,4 @@
-public class Acertijo {  // public abstract class? creo que lo vemos mas adelante.
+public abstract class Acertijo {  // public abstract class? creo que lo vemos mas adelante.
 
 // -- METODOS --
 
@@ -33,5 +33,33 @@ public class Acertijo {  // public abstract class? creo que lo vemos mas adelant
     }
 
 // -- METODOS --
+
+    public void mostrarEnunciado () {
+        System.out.println(this.descripcion);
+    }
+
+    public boolean validarRespuesta (String respuesta) {
+        if (this.resuelto) {
+            return true;
+        }
+        boolean esCorrecta = verificarRespuesta(respuesta);
+        if (esCorrecta) {
+            this.resuelto = true;
+        }
+            return esCorrecta;
+    }
+
+    // -- METODOS ABSTRACTOS --
+
+    protected abstract boolean verificarRespuesta(String respuesta);
+
+    // -- PARTE INTERACTUABLE --
+    @Override
+    public void interactuar(Personaje perso) {
+        mostrarEnunciado();
+        // TODO: Aca se debe mostrar un cuadro de texto para que el jugador escriba su respuesta
+    }
+
+
 
 }
