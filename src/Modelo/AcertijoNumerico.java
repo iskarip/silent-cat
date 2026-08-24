0161a070c9edb67ad59cbb5f7f9430a60a2275f6
Iskari Pardo <iskaripardo444@gmail.com>
2026-08-23 23:07:42 -0300

    Avance acertijoNumerico.java, Agregue Item.java y ItemBateria

---
+++ src/Modelo/AcertijoNumerico.java
@@ -0,0 +1,27 @@
+public class AcertijoNumerico extends Acertijo{
+
+// -- ATRIBUTOS -- 
+
+    private int respuesta;
+
+// -- CONSTRUCTOR --
+
+    public AcertijoNumerico (int id, String descripcion, int respuesta){
+        super(id, descripcion);
+        this.respuesta = respuesta;
+    }
+
+// -- GET y SET
+
+// -- METODOS --
+
+    @Override
+    public boolean validarRespuesta(String respuesta) {
+        int intento = Integer.parseInt(respuesta); // convertís el String a int
+        this.resuelto = (intento == this.respuesta);
+        return resuelto;
+    }
+
+    }
+
+
---
+++ src/Modelo/itemBateria.java
@@ -0,0 +1,21 @@
+public class itemBateria extends Item {
+
+// -- ATRIBUTOS --
+    
+    private int cantidadCarga;
+
+// -- CONSTRUCTOR --
+    public itemBateria(){
+        super();
+        cantidadCarga = 10;
+    }
+
+// -- GET y SET --
+
+    public int getCantidadCarga(){
+        return cantidadCarga;
+    }
+
+// --  METODOS --
+    
+}
