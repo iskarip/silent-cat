7ac817e8b2cbf17d7a91562fffac8bc1b54692ce
Aldana Godoy <godoyaldanasofia@gmail.com>
2026-08-29 22:03:58 -0300

    Modifique JuegoFrame

--- src/Vista/JuegoFrame.java
+++ src/Vista/JuegoFrame.java
@@ -1,6 +1,36 @@
 package Vista;
+import javax.swing.*;
+import java.awt.*;
 
-public class JuegoFrame {
+
+public class JuegoFrame extends JFrame {
+
+    private CardLayout cardLayout;
+    private JPanel panelContenedor;
+
+    public JuegoFrame() {
+        setTitle("Silent Cat");
+        setSize(800, 600);
+        setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+
+        cardLayout = new CardLayout();
+        panelContenedor = new JPanel(cardLayout);
+
+        panelContenedor.add (new MenuPanel(this), "menu");
+        panelContenedor.add (new SeleccionPersonajePanel(this), "seleccion");
+
+        add(panelContenedor);
+
+        setVisible(true);
+
+    }
+    // Este método es el que van a llamar desde cualquier pantalla
+    // para pedirle a la ventana "mostrame otra carta"
+    public void mostrarPantalla (String pantalla){
+        cardLayout.show(panelContenedor, pantalla);
+    }
+
+
 /*  La ventana contenedora principal
     Usa un CardLayout para cambiar entre pantallas 
     (Menú, Selección, Nivel, etc.) */
