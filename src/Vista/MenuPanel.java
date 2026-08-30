8f1db6b74f39cc8f66a79594c3d8593fcbbf45a8
Iskari Pardo <iskaripardo444@gmail.com>
2026-08-29 22:02:28 -0300

    Cuerpo inicial en Menu.java

--- src/Vista/MenuPanel.java
+++ src/Vista/MenuPanel.java
@@ -1,11 +1,25 @@
 package Vista;
 
-public class MenuPanel {
-/* El menu de inicio con botones
-(iniciar juego, salir) */ 
-    
-/*podemos empezar con un panel simple con titulo y boton para empezar
-que le avisa a juegoFrame que cambie de pantalla */
+import javax.swing.*;
+import java.awt.event.ActionEvent;
 
+public class MenuPanel extends JPanel {
 
+    private JuegoFrame ventanaPrincipal;
+
+    public MenuPanel(JuegoFrame ventanaPrincipal) {
+        this.ventanaPrincipal = ventanaPrincipal;
+
+        JButton botonIniciar = new JButton("Iniciar");
+        botonIniciar.addActionListener(e -> {
+            ventanaPrincipal.mostrarPantalla("seleccion");
+        });
+
+        JButton botonSalir = new JButton("Salir");
+        botonSalir.addActionListener(e -> System.exit(0));
+
+        add(botonIniciar);
+        add(botonSalir);
+    }
 }
+
